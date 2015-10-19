4576f5be6d573c69056c6a191321921c918d27d9
RJjava <851937235@qq.com>
2015-10-19 11:49:27 +0800

    我第一个GitHub程序

---
+++ FistGitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FistGitHubTest
+//
+//  Created by echo13 on 15/10/19.
+//  Copyright (c) 2015年 echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FistGitHubTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FistGitHubTest
+//
+//  Created by echo13 on 15/10/19.
+//  Copyright (c) 2015年 echo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
