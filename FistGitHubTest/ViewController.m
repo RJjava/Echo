5ae22a70025473cc9f997128f0d59dd7d4536025
RJjava <851937235@qq.com>
2015-10-19 12:12:54 +0800

    添加了2个输出

--- FistGitHubTest/ViewController.m
+++ FistGitHubTest/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"我是好人");
+    NSLog(@"我真的是好人");
 }
 
 - (void)didReceiveMemoryWarning {
